a5e6bb7d236c815ac1ddd8c3ec202453f8359a15
gabrielrodrigolinhares <gabriel.rodrigo.linhares@escola.pr.gov.br>
2026-03-09 13:26:09 +0000

    introdução aos arrays in js

---
+++ arrays/array-time.js
@@ -0,0 +1,8 @@
+// Array:são agrupamentos de valores em forma de list
+// são idexados a partir de ZERO até o infinito
+// podem ter valores homegêneos/heterogêneos
+// possuem métodos (funções) próprias
+
+const time = ['petter', 'mariah']
+  // indeices   0   1
+  console.log(time{1})
